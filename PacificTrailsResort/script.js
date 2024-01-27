7aa93714efd1a6cbee9ce4b40561294c1bc88016
Caty1121 <catherine.yount@g.austincc.edu>
2024-01-27 14:58:10 -0500

    JS is working and icons can drag and drop to the cart

---
+++ PacificTrailsResort/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    let draggables = document.querySelectorAll('.draggable');
+    let dropzone = document.getElementById('cart-container');
+
+    draggables.forEach(draggable => {
+        draggable.addEventListener('dragstart', function(event) {
+            event.dataTransfer.setData("text", event.target.id);
+        });
+    });
+
+    dropzone.addEventListener('dragover', function(event) {
+        event.preventDefault(); 
+    });
+
+    dropzone.addEventListener('drop', function(event) {
+        event.preventDefault();
+        let data = event.dataTransfer.getData("text");
+        let nodeCopy = document.getElementById(data).cloneNode(true);
+        event.target.appendChild(nodeCopy);
+    });
+});
